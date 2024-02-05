f151caeb498dddc0dd46c54ed264a18fd42a4b63
lvirrueta <luis510vg@gmail.com>
2024-02-04 18:53:57 -0600

    add constants & interface

---
+++ client/src/app/constants/word-seek.constants.ts
@@ -0,0 +1,63 @@
+import { IWord2Seek } from "../interface/word-seek.interface";
+
+export const wordSeek = [
+  ['I', 'M', 'X', 'F', 'M', 'A', 'J', 'N', 'L', 'Y', 'G', 'B', 'I','O', 'O'],
+  ['J', 'V', 'O', 'R', 'U', 'B', 'I', 'O', 'F', 'N', 'N', 'Y', 'A','T', 'F'],
+  ['C', 'L', 'V', 'O', 'U', 'M', 'T', 'L', 'Y', 'L', 'B', 'H', 'D','W', 'J'],
+  ['H', 'U', 'V', 'N', 'H', 'O', 'C', 'A', 'Y', 'O', 'B', 'D', 'Y','I', 'G'],
+  ['C', 'O', 'V', 'T', 'R', 'D', 'B', 'Z', 'T', 'U', 'U', 'P', 'C','E', 'U'],
+  ['E', 'X', 'A', 'C', 'A', 'G', 'H', 'R', 'G', 'F', 'P', 'E', 'O','H', 'E'],
+  ['Q', 'X', 'Y', 'V', 'P', 'L', 'I', 'N', 'E', 'Y', 'X', 'V', 'C','W', 'P'],
+  ['O', 'B', 'O', 'M', 'O', 'H', 'K', 'O', 'E', 'H', 'M', 'H', 'O','M', 'A'],
+  ['J', 'X', 'L', 'P', 'E', 'T', 'D', 'E', 'Z', 'T', 'T', 'F', 'D','G', 'R'],
+  ['D', 'E', 'T', 'N', 'L', 'W', 'W', 'L', 'S', 'A', 'I', 'G', 'R','W', 'D'],
+  ['I', 'M', 'X', 'F', 'M', 'A', 'J', 'N', 'L', 'Y', 'G', 'B', 'I','O', 'O'],
+  ['C', 'C', 'P', 'O', 'I', 'U', 'Y', 'I', 'Í', 'B', 'R', 'J', 'L','G', 'K'],
+  ['O', 'R', 'Z', 'A', 'W', 'Z', 'U', 'T', 'I', 'F', 'E', 'L', 'O','T', 'G'],
+  ['Q', 'A', 'M', 'U', 'P', 'G', 'D', 'O', 'R', 'K', 'L', 'C', 'I','V', 'N'],
+  ['S', 'N', 'K', 'N', 'A', 'Q', 'P', 'G', 'C', 'X', 'H', 'E', 'J','D', 'F'],
+  ['Z', 'S', 'P', 'F', 'M', 'L', 'P', 'S', 'S', 'Z', 'T', 'K', 'D','L', 'G'],
+];
+
+export const word2Seek: IWord2Seek[] = [
+  {
+    color: '#FF0000',
+    word: 'TIBURON',
+  },
+  {
+    color: '#00FF00',
+    word: 'LEOPARDO',
+  },
+  {
+    color: '#0000FF',
+    word: 'PUMA',
+  },
+  {
+    color: '#FF00FF',
+    word: 'COCODRILO',
+  },
+  {
+    color: '#00FFFF',
+    word: 'LEON',
+  },
+  {
+    color: '#FFFF00',
+    word: 'DELFIN',
+  },
+  {
+    color: '#AA3300',
+    word: 'TIGRE',
+  },
+  {
+    color: '#AAFF00',
+    word: 'AGUILA',
+  },
+  {
+    color: '#00AAFF',
+    word: 'LOBO',
+  },
+  {
+    color: '#88AA66',
+    word: 'GUEPARDO',
+  },
+]
